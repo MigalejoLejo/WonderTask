13ac2bfaaee4390c22603b4dcbaf1ceca2025b7a
MigalejoLejo <migalejo85@gmail.com>
2025-11-26 17:06:38 +0100

    init project with first set up configuration

---
+++ WonderTask-BackEnd/src/main/java/com/nxdlab/WonderTask/WonderTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nxdlab.WonderTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderTaskApplication.class, args);
+	}
+
+}
